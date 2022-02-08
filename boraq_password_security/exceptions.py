bd82096749a441128b2a3c4bf962cdfdacf7b361
Oneil Clacken <oneilclacken@yahoo.com>
2022-02-07 22:39:21 -0500

    [ADD] app boraq_password_security

---
+++ boraq_password_security/exceptions.py
@@ -0,0 +1,10 @@
+# Copyright 2015 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo.exceptions import UserError
+
+
+class PassError(UserError):
+    """ Example: When you try to create an insecure password."""
+    def __init__(self, msg):
+        raise UserError(msg)
